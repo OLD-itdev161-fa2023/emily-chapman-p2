4ac1788425d5c317724be304682e3dd3cd57c7a4
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-11-19 10:14:02 -0600

    Created Register Baker API Endpoint

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 import express from 'express';
 import connectDatabase from './config/database';
+import {check, validationResult} from 'express-validator';
 
-//Initialize express application
+//Initialize Express Server
 const recipeApp = express();
 
 //Connect to Database
 connectDatabase();
 
+//Configure Middleware
+recipeApp.use(express.json({ extended: false }));
+
 //Create root API endpoint
 recipeApp.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
+);
+
+//Create register API endpoint
+recipeApp.post(
+    '/api/bakers', 
+    [
+        check('name', 'Please enter your name.').not().isEmpty(),
+        check('email', 'Please enter a valid email.').isEmail(),
+        check('password', 'Please enter a password with six or more characters.').isLength({min: 6}),
+        check('favoriteDessert', 'Please enter your favorite dessert.').not().isEmpty()
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        } else {
+            res.send(req.body);
+        }
+    }
 );
 
 //Set up connection listener
