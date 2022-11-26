a471396d806314ce6d5d082d6036c22a6579b27f
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-11-26 11:58:09 -0600

    Built Out Login Component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 
-const Login = () => {
-    return <div>Login</div>;
+const Login = ({authenticateBaker}) => {
+    let history = useHistory();
+    
+    const [bakerData, setBakerData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [errorData, setErrorData] = useState({errors: null});
+
+    const {email, password} = bakerData;
+    const {errors} = errorData;
+
+    const onChange = e => {
+        const {name, value} = e.target;
+        setBakerData ({
+            ...bakerData,
+            [name]: value
+        })
+    }
+
+    const loginBaker = async () => {
+        const newBaker = {
+            email: email,
+            password: password
+        }
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+
+            const body = JSON.stringify(newBaker);
+            const res = await axios.post('http://localhost:5000/api/login', body, config);
+            
+            //Store baker data and redirect
+            localStorage.setItem('token', res.data.token);
+            history.push('/');
+        } catch (error) {
+            //Clear baker data and set errors
+            localStorage.removeItem('token');
+                
+            setErrorData({
+                ...errors,
+                errors: error.response.data.errors
+            })
+        }
+
+        authenticateBaker();
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={email}
+                    onChange={e => onChange(e)}
+                />
+            </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={e => onChange(e)}
+                />
+            </div>
+            <div>
+                <button onClick={() => loginBaker()}>Login</button>
+            </div>
+            <div>
+                {errors && errors.map(error => 
+                    <div key={error.msg}>{error.msg}</div>)}
+            </div>
+        </div>
+    )
 }
 
 export default Login;
